fb05f4bd532b28a66f6b721f49023f63a8b37f63
sudevkrishna <sudevkrishnat77@gmail.com>
2021-05-26 09:30:48 +0530

    Modified

--- App/urls.py
+++ App/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('test',views.test,name='test')
+    path('test',views.test,name='test'),
+    path('test1',views.test1,name='test1'),
+    path('test2',views.test2,name='test2'),
+    path('final',views.final,name='final')
 ]
--- App/views.py
+++ App/views.py
@@ -2,3 +2,9 @@ from django.shortcuts import render
 
 def test(request):
     return render(request,'samplewebpage.html')
+def test1(request):
+    return render(request,'gridex.html')
+def test2(request):
+    return render(request,'ex2.html')
+def final(request):
+    return render(request,'FinalProject.html')
